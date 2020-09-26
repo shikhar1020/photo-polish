a8925c856cfbaac7d085b688d3c399094b4d49ab
Shikhar <shikhar.17.srivastava@st.niituniversity.in>
2020-09-26 06:01:23 +0530

    Sidebar Styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 
 import Slider from './Slider';
+import SidebarItems from './SidebarItems'
 
 function App() {
   return (
     <div className="appPage">
       <div className="image" />
         <div className="sidebar">
-
-        This is for Sidebar Bro!
+        <SidebarItems />
+        <SidebarItems />
+        <SidebarItems />
         </div>      
       <Slider />
     </div>
---
+++ src/SidebarItems.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './sidebarItems.css'
+
+function SidebarItems() {
+    return (
+        <button className="sidebarItem "> Polish Function </button>
+    )
+}
+
+export default SidebarItems
